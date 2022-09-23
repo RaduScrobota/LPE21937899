59978ab34017e48b0243e97adc183c08500e8443
Radu Scrobota <raduscrobota@MacBook-Pro-de-Radu.local>
2022-09-23 11:24:38 +0200

    created repo

---
+++ Hands on 01 .R
@@ -0,0 +1,19 @@
+
+# ID SCRIPT ---------------------------------------------------------------
+
+##LENGUAJES DE PROGRAMACIÓN ESTADÍSTICA
+## PROFESOR: CHRISTIAN SUCUZHANAY AREVALO 
+## ALUMNO: RADU SCROBOTA, EXP 21937899
+## HANDS ON 01
+
+
+# SHORTCUTS ---------------------------------------------------------------
+
+## control + l = clean console
+## control + shift + r = nueva sección
+
+
+# LOADING LIBS ------------------------------------------------------------
+
+install.packages("tidyverse","httr","janitor")
+
